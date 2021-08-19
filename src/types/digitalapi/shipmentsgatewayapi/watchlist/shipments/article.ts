202e879f12f2b27ab307b42ae7943526544005d2
quinn <quinn.perry@futurestatesystems.com>
2021-08-19 19:55:16 +1000

    Add type for /shipments endpoint and example JSON

---
+++ src/types/digitalapi/shipmentsgatewayapi/watchlist/shipments/article.ts
@@ -0,0 +1,56 @@
+type shipmentsResponse = {
+    status: 'Success' | /*?*/'Failure';
+    consignmentId: string; // number ? 
+    articles: article[];
+}
+
+type article = {
+    articleId: string;
+    status: status;
+    altStatus: string | null;
+    trackStatusOfArticle: string;
+    statusColour: string;
+    redirectIneligibility: codeReason;
+    safeDropIneligibility: codeReason;
+    details: detail[];
+}
+
+type detail = {
+    articleId: string;
+    consignmentId: string;
+    articleType: string;
+    productSubType: string;
+
+    internationalTracking: any;
+    estimatedDeliveryDateEligible: boolean;
+    signatureOnDelivery: signatureOnDelivery;
+
+    events: event[];
+}
+
+type event = {
+    dateTime: number; // local time
+    localeDateTime: string; // cast to Datee
+    description: string;
+    location: string;
+    eventCode: string;
+    wcid: string;
+}
+type signatureOnDelivery = {
+    required: boolean;
+    instruction: {
+        code: 'SIG_NO',
+        description: string | null;
+    }
+}
+
+type status = {
+    statusAttributeName: string;
+    statusAttributeValue: 'Delivered';
+    statusModificationTime: number;
+}
+
+type codeReason = {
+    code: string; // number ? 
+    reason: string;
+}
