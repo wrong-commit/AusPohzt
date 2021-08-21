2419e03ba143268778b10f6686a44e1f6a5a2564
quinn <quinn.perry@futurestatesystems.com>
2021-08-21 13:10:20 +1000

    Add barebones parcel class

---
+++ src/models/parcel.ts
@@ -0,0 +1,59 @@
+import { Dto } from "../types/Dto";
+
+export { parcel };
+/**
+ * class object for parcel being tracked
+ */
+class parcel {
+    /**
+     * Unique ID of parcel. 
+     */
+    id: string;
+
+    /**
+     * digitalapi tracking ID.
+     */
+    trackingId: string;
+
+    /**
+     * Id of user that owns this package. 
+     * 
+     * TODO: spec this out. 
+     */
+    owner: number;
+    /**
+     * Use defined nickname. Can come from digitalapi or user defined.
+     */
+    nickName?: string;
+
+    /**
+     * Last time parcel synced, unix Epoch.
+     */
+    lastSync: number;
+
+    /**
+     * TODO: use real type.
+     */
+    events: string[];
+
+    constructor(data: Dto<parcel>) {
+        this.id = data.id;
+        this.trackingId = data.trackingId;
+        this.owner = data.owner;
+
+        this.nickName = data.nickName;
+        this.events = data.events;
+        this.lastSync = data.lastSync;
+    }
+
+    toData(): Dto<parcel> {
+        return {
+            id: this.id,
+            owner: this.owner,
+            trackingId: this.trackingId,
+            nickName: this.nickName,
+            lastSync: this.lastSync,
+            events: this.events,
+        }
+    }
+}
