786491ee9e4cc4628194120eae757932cafd7263
quinn <quinn.perry@futurestatesystems.com>
2021-08-26 23:52:45 +1000

    Add dao, parcelDao

---
+++ src/dao/dao.ts
@@ -0,0 +1,47 @@
+import { QueryResult } from "pg";
+
+export { dao }
+abstract class dao<T> {
+
+    abstract findAll(): PromiseLike<T[] | undefined>;
+    /**
+     * Lookup object
+     * @param id 
+     */
+    abstract find(id: any): PromiseLike<T | undefined>;
+    /**
+     * Create new or update existing of type {T}
+     * @param value 
+     */
+    abstract save(value: T): PromiseLike<T>;
+
+    /**
+     * Delete object by Id
+     * @param id 
+     */
+    abstract delete(id: any): PromiseLike<boolean>;
+
+    // TODO: move somewhere else
+    /**
+     * 
+     * @param result
+     * @param count Expected rows in result
+     * @throws Error if more than one row returned
+     */
+    expectedRows(result: QueryResult, count: number) {
+        if (result.rowCount !== count) {
+            const msg = result.rowCount === 0 ? 'No rows returned' : `More than ${count} row${count === 1 ? '' : 's'} returned`;
+            throw new Error(msg);
+        }
+        return result;
+    }
+
+    // TODO: add Error Logging method
+    /**
+    if (err === undefined) {
+        console.log("No errors returned from Postgres")
+    } else if (err.message !== undefined) {
+        console.log('ERROR message:', err.message)
+    }
+     */
+}
