5984d8f9be600d983f9ea64ba216961e7f34d5c0
quinn <quinn.perry@futurestatesystems.com>
2021-08-27 00:22:07 +1000

    Add parcelDao.findByTrackingId

--- src/dao/parcelDao.ts
+++ src/dao/parcelDao.ts
@@ -35,7 +35,16 @@ class parcelDao extends dao<parcel> {
             return undefined;
         }
     }
-
+    async findByTrackingId(trackingId: string): Promise<parcel | undefined> {
+        try {
+            const result = await pool.query('SELECT * FROM parcel WHERE trackingId = $1', [trackingId]);
+            this.expectedRows(result, 1);
+            return new pirate<parcel>(result.rows[0], result.fields).map(parcel);
+        } catch (e) {
+            console.error(`Could not find parcel with trackingId ${trackingId}`, e);
+            return undefined;
+        }
+    }
     async save(value: parcel): Promise<parcel> {
         console.log(value)
         throw new Error("Method not implemented.");
