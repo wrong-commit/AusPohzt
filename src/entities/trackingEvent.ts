e3bb15579db3a4b446f9165130d47c3fe0f8cd3a
quinn <quinn.perry@futurestatesystems.com>
2021-08-29 02:33:40 +1000

    Setup trackingEvent binds

--- src/entities/trackingEvent.ts
+++ src/entities/trackingEvent.ts
@@ -1,4 +1,4 @@
-import { entity } from "../decorator/entityDecorators";
+import { bind, entity } from "../decorator/entityDecorators";
 import { Dto } from "../types/Dto";
 
 export { trackingEvent, trackingEventStatus }
@@ -20,40 +20,48 @@ type trackingEventStatus =
 @entity('trackingEvent')
 class trackingEvent {
     // is id necessary ? only if updating or deleting single entries at a time. otherwises delete where parcelId = $1
+    @bind
     id?: number;
 
     /**
      * JOIN column target to link to {@link parcel} 
      */
+    @bind
     parcelId?: number;
 
     /**
      * Unix Epoch of when event occurred.
      */
+    @bind
     dateTime: number;
 
     // TODO: spec
+    @bind
     location: string;
 
     /**
      * Message associated with tracking event. 
      * Combine all header + description info into this property.
      */
+    @bind
     message: string;
 
     // TODO: move to parcel ? 
     /**
      * Types of tracking updates we care about.
      */
+    @bind
     type: trackingEventStatus;
 
     /**
      * Store raw response as JSON. 
      */
+    @bind
     raw: string;
 
     constructor(data: Dto<trackingEvent>) {
         this.id = data.id;
+        this.parcelId = data.parcelId;
         this.dateTime = data.dateTime;
         this.location = data.location;
         this.message = data.message;
