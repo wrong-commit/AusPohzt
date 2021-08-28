04063da82b6df1f4d0a71785c715141d5ff981cc
quinn <quinn.perry@futurestatesystems.com>
2021-08-28 21:51:43 +1000

    Add specific trackingEvent types Add specific codes from article (could use prefixes instead ?)

--- src/entities/trackingEvent.ts
+++ src/entities/trackingEvent.ts
@@ -1,13 +1,29 @@
 import { Dto } from "../types/Dto";
 
-export { trackingEvent }
+export { trackingEvent, trackingEventStatus }
 
+type trackingEventStatus =
+    /* Something went wrong */
+    'failed' |
+    /* Not yet sent, status not yet able to be recalculated */
+    'pending' |
+    /* Out for delivery */
+    'in transit' |
+    /* Awaiting collection, pickup from location ? */
+    'awaiting collection' |
+    /* Delivered */
+    'delivered';
 /**
  * event updates for tracked item
  */
 class trackingEvent {
-    // is id necessary ? 
-    id: number;
+    // is id necessary ? only if updating or deleting single entries at a time. otherwises delete where parcelId = $1
+    id?: number;
+
+    /**
+     * JOIN column target to link to {@link parcel} 
+     */
+    parcelId?: number;
 
     /**
      * Unix Epoch of when event occurred.
@@ -23,10 +39,16 @@ class trackingEvent {
      */
     message: string;
 
+    // TODO: move to parcel ? 
     /**
      * Types of tracking updates we care about.
      */
-    type: 'pending' | 'delivering' | 'delivered';
+    type: trackingEventStatus;
+
+    /**
+     * Store raw response as JSON. 
+     */
+    raw: string;
 
     constructor(data: Dto<trackingEvent>) {
         this.id = data.id;
@@ -34,6 +56,7 @@ class trackingEvent {
         this.location = data.location;
         this.message = data.message;
         this.type = data.type;
+        this.raw = data.raw;
     }
 
     toData(): Dto<trackingEvent> {
@@ -43,6 +66,7 @@ class trackingEvent {
             location: this.location,
             message: this.message,
             type: this.type,
+            raw: this.raw,
         };
     }
 }
