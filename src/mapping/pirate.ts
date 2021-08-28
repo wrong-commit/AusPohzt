94cfe687d030293284dc36b86054a1a75871d984
quinn <quinn.perry@futurestatesystems.com>
2021-08-28 23:34:17 +1000

    Pirate instantiates @join properties with [] or {} Commit tsconfig changes to load JSON test data

--- src/mapping/pirate.ts
+++ src/mapping/pirate.ts
@@ -1,50 +1,67 @@
 import { FieldDef, QueryResultRow } from "pg";
 import { getEntityName, getFields } from "../decorator/entityDecorators";
+import { getJoinData, joinData } from "../decorator/joinDecorator";
 import { Dto } from "../types/Dto";
 import { DtoNewable } from "../types/DtoNewable";
 
 export { pirate };
+
+
+type JoinQueryResult = {
+    row: QueryResultRow,
+    fieldDefs: FieldDef[]
+};
 class pirate<T extends object> {
     row: QueryResultRow | QueryResultRow[];
     fieldDefs: FieldDef[];
-    constructor(row: QueryResultRow | QueryResultRow[], fieldDefs: FieldDef[]) {
+    joinResults?: JoinQueryResult[];
+
+    constructor(row: QueryResultRow, fieldDefs: FieldDef[]);
+    constructor(row: QueryResultRow[], fieldDefs: FieldDef[]);
+    constructor(row: QueryResultRow, fieldDefs: FieldDef[], joinResults: JoinQueryResult[]);
+
+    // populate with target: DtoNewable<T> instead of query row results.
+    constructor(row: any, fieldDefs: any, joinResults?: any) {
         this.row = row;
         this.fieldDefs = fieldDefs;
+        this.joinResults = joinResults;
     }
 
     /**
      * Map from ResultQuery to constructor argument for instance.
      */
     map(target: DtoNewable<T>): T {
-        const entityName = getEntityName(target);
-        console.debug(`Mapping entity ${entityName}`)
-        const fields = getFields(target);
-
-        let row;
         if (Array.isArray(this.row)) {
-            row = this.row[0];
-        } else {
-            row = this.row;
+            throw new Error('Cannot map single entity when multiple rows returned. Use pirate.mapMany()')
         }
+
+        return this._map(target, this.row);
+    }
+    mapMany(target: DtoNewable<T>): T[] {
+        if (!Array.isArray(this.row)) {
+            throw new Error('Cannot map many entities when pirate constructed with single QueryResultRow. Uses pirate.map()');
+        }
+        return this.row.map(row => this._map(target, row));
+    }
+
+    /**
+     * Performs actual mapping
+     */
+    private _map(target: DtoNewable<T>, row: QueryResultRow): T {
+        const entityName = getEntityName(target);
+        const fields = getFields(target);
+        const joins = getJoinData(target);
+        console.debug(`Mapping ${fields.length} fields, ${joins?.length} joins to entity ${entityName}`);
         //@ts-expect-error
         let dto: Dto<T> = {}
         this.mapFields(row, fields, dto);
 
+        if (joins) {
+            this.mapJoins(joins, dto);
+        }
+
         // // create instance of type T 
         return new target(dto);
-    }
-    mapMany(target: DtoNewable<T>): T[] {
-        const entityName = getEntityName(target);
-        console.debug(`Mapping entity ${entityName}`)
-        const fields = getFields(target);
-
-        let rows;
-        if (Array.isArray(this.row)) {
-            rows = this.row;
-        } else {
-            rows = [this.row];
-        }
-        return rows.map(row => this.mapFields(row, fields, {} as Dto<T>)).map(dto => new target(dto));
     }
 
     private mapFields(row: undefined | QueryResultRow, fields: string[], dto: Dto<T>): Dto<T> {
@@ -60,6 +77,28 @@ class pirate<T extends object> {
             }
             //@ts-expect-error
             dto[field] = row[field.toLowerCase()];
+        }
+
+        return dto;
+    }
+
+    private mapJoins(joins: joinData[], dto: Dto<T>): Dto<T> {
+
+        for (const [field, entity, assoc] of joins) {
+            console.debug(`Field ${field} joins to ${assoc} entity ${entity}`);
+            // // find fieldDef
+            // const fieldDef: undefined | FieldDef = this.fieldDefs.find(x => x.name === field.toLowerCase());
+            // if (!fieldDef) {
+            //     console.warn(`Could not find returned field for ${field}`);
+            //     continue;
+            // }
+            if (assoc === 'multiple') {
+                //@ts-expect-error
+                dto[field] = [];
+            } else {
+                //@ts-expect-error
+                dto[field] = null;
+            }
         }
 
         return dto;
