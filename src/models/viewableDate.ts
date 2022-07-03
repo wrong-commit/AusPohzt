025edf6ce7a23123a421a54eb2f31191489a39d7
quinn <quinn.samuel.perry@gmail.com>
2022-07-03 15:54:35 +1000

    Add viewableDate function to disply dates nice

---
+++ src/models/viewableDate.ts
@@ -0,0 +1,21 @@
+export { viewableDate }
+/**
+ * Output human readable date. Provides consistent styling for all FE Dates.
+ * @param input {Date | String | Number} input object
+ */
+const viewableDate = (input: Date | Number): String => {
+    if (input instanceof Date) {
+        // convert to format. YYYY-MM-DD HH:MM
+        return `${input.getFullYear()}-${input.getMonth() + 1}-${input.getDate()} ${padTime(input.getHours())}:${padTime(input.getMinutes())}`;
+    } else if (input instanceof Number || Number.parseInt(input + '') === (input as unknown as number)) {
+        // assume Number is epoch in MS.
+        return viewableDate(new Date(input as number));
+        // } else if (input instanceof String) {
+        // how tf do we process a String ??? 
+    }
+    return '';
+}
+
+function padTime(input: number): string {
+    return input < 10 ? '0' + input : input + '';
+}
