083adffcbc5c422176b6fce9d8f9bacf6e61170f
quinn <quinn.samuel.perry@gmail.com>
2022-07-03 15:54:38 +1000

    Modify DTO type to recursively DTOify arrays/objects

--- src/types/Dto.ts
+++ src/types/Dto.ts
@@ -1,5 +1,18 @@
-import { NonMethodKeys } from "./NonMethodKeys"
+import { parcel } from "../entities/parcel";
+import { NonMethodKeys } from "./NonMethodKeys";
 
-export { Dto }
+export { Dto };
 
-type Dto<T> = Pick<T, NonMethodKeys<T>>;
+type DtoProperties<T> = {
+    // K is property of type T
+    [K in keyof T]:
+    // change Array<U> to Array<Dto<U>>
+    T[K] extends Array<infer U> ? Array<Dto<U>> :
+    // change object to Dto<>
+    T[K] extends object ? Dto<T[K]>
+    // primatives
+    : T[K]
+};
+
+// remove Method keys from DtoProperties
+type Dto<T> = Pick<DtoProperties<T>, NonMethodKeys<T>>
