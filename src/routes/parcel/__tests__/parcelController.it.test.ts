2e20d764e112ecd0141d550eeaedf9a340dad560
quinn <quinn.samuel.perry@gmail.com>
2022-07-03 15:54:36 +1000

    Add endpoints to delete, find parcels

--- src/routes/parcel/__tests__/parcelController.it.test.ts
+++ src/routes/parcel/__tests__/parcelController.it.test.ts
@@ -1,21 +1,50 @@
 import request from 'supertest';
 import { buildExpress } from '../../../buildExpress';
 import { pool } from '../../../database/database';
+import { parcel } from '../../../models/parcel';
+import { Dto } from '../../../types/Dto';
+
+let findParcel: number;
+// parcel to delete
 describe("parcelController", () => {
     let app = buildExpress();
     beforeAll(async () => {
-        await pool.query(
-            `INSERT INTO parcel (trackingId, owner, nickname, lastSync) VALUES ('testTrackingId', 1, 'nickname', 0);`
-        );
+        findParcel = (await pool.query(
+            `INSERT INTO parcel (trackingId, owner, nickname, lastSync) VALUES ('findParcelTrkId', 1, 'nickname', 0) RETURNING id;`
+        )).rows[0]['id'];
     })
     // FIXME: wtf, why is async jest broken in this version ? need to reread documentation so --forceExit isn't required
-    it("get all parcels", async () => {
+    test("get all parcels", async () => {
         await request(app)
             .get('/v0/parcel/')
             .expect('Content-Type', /json/)
             .expect(200)
             .then(resp => {
-                expect(resp.body).toStrictEqual(['testTrackingId']);
+                expect(resp.body.map((x: Dto<parcel>) => x.trackingId)).toContain('findParcelTrkId');
             })
     });
-})
+
+    test("get parcel by id", async () => {
+        await request(app)
+            .get(`/v0/parcel/${findParcel}`)
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then(resp => {
+                expect(resp.body.trackingId).toBe('findParcelTrkId');
+            })
+    });
+
+    test("delete parcel", async () => {
+        let deleteParcel = (await pool.query(
+            `INSERT INTO parcel (trackingId, owner, nickname, lastSync) VALUES ('deleteParcelTrkId', 1, 'nickname', 0) RETURNING id;`
+        )).rows[0]['id'];
+
+        await request(app)
+            .delete(`/v0/parcel/${deleteParcel}`)
+            .expect(200);
+
+        await request(app)
+            .delete(`/v0/parcel/${deleteParcel}`)
+            .expect(404);
+    });
+});
