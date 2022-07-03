d7e4a6c4af44976977609eb44cd9a89f1ac8e9f3
quinn <quinn.samuel.perry@gmail.com>
2022-07-03 15:54:39 +1000

    Support undefined single association joins

--- src/dao/__tests__/dao.test.ts
+++ src/dao/__tests__/dao.test.ts
@@ -29,86 +29,142 @@ class _book {
 }
 
 describe("dao", () => {
-    describe("single association joins", () => {
-        test("single association saves", async () => {
-            // mocked saves to return id 1
-            const AUTHOR_ID = 1;
-            const BOOK_ID = 2;
-            jest.spyOn(pool, 'query')
-                .mockImplementationOnce((): PromiseLike<Partial<QueryResult>> =>
-                    Promise.resolve({
-                        rowCount: 1,
-                        rows: [{
-                            'id': BOOK_ID
-                        }]
-                    })
-                ).mockImplementationOnce((): PromiseLike<Partial<QueryResult>> =>
-                    Promise.resolve({
-                        rowCount: 1,
-                        rows: [{
-                            'id': AUTHOR_ID
-                        }]
-                    })
-                ).mockImplementation(() => Promise.reject('mock not implemented'));
+    beforeEach(() => {
+        mockedPool.clearAllMocks()
+    })
+    describe("join saves", () => {
+        describe("single association", () => {
+            test("id set on joined entity", async () => {
+                // mocked saves to return id 1
+                const AUTHOR_ID = 1;
+                const BOOK_ID = 2;
+                jest.spyOn(pool, 'query')
+                    .mockImplementationOnce((): PromiseLike<Partial<QueryResult>> =>
+                        Promise.resolve({
+                            rowCount: 1,
+                            rows: [{
+                                'id': BOOK_ID
+                            }]
+                        })
+                    ).mockImplementationOnce((): PromiseLike<Partial<QueryResult>> =>
+                        Promise.resolve({
+                            rowCount: 1,
+                            rows: [{
+                                'id': AUTHOR_ID
+                            }]
+                        })
+                    ).mockImplementation(() => Promise.reject('mock not implemented'));
 
-            // call save
-            const dao = daoFactory<_book>(_book, pool);
+                // call save
+                const dao = daoFactory<_book>(_book, pool);
 
-            let book = new _book();
-            let author = new _author();
-            book.author = author;
+                let book = new _book();
+                book.author = new _author();
 
-            book = (await dao.save(book))!;
-            expect(book).toBeDefined();
-            expect(book.id).toBe(BOOK_ID);
-            expect(book.author?.id).toBe(AUTHOR_ID);
-        })
+                book = (await dao.save(book))!;
+                expect(book).toBeDefined();
+                expect(book.id).toBe(BOOK_ID);
+                expect(book.author?.id).toBe(AUTHOR_ID);
+            })
 
-        test("multiple association saves", async () => {
-            // mocked saves to return id 1
-            const BOOK_ID = 1;
-            const PAGE_ID = 2;
-            const PAGE_ID_2 = 3;
-            jest.spyOn(pool, 'query')
-                .mockImplementationOnce((): PromiseLike<Partial<QueryResult>> =>
-                    Promise.resolve({
-                        rowCount: 1,
-                        rows: [{
-                            'id': BOOK_ID
-                        }]
-                    })
-                )
-                .mockImplementationOnce((): PromiseLike<Partial<QueryResult>> =>
-                    Promise.resolve({
-                        rowCount: 1,
-                        rows: [{
-                            'id': PAGE_ID
-                        }]
-                    })
-                )
-                .mockImplementationOnce((): PromiseLike<Partial<QueryResult>> =>
-                    Promise.resolve({
-                        rowCount: 1,
-                        rows: [{
-                            id: PAGE_ID_2
-                        }]
-                    })
-                ).mockImplementation(() => { Promise.reject('mock not implemented') })
+            test("undefined join object ignored", async () => {
+                // mocked saves to return id 1
+                const BOOK_ID = 2;
+                jest.spyOn(pool, 'query')
+                    .mockImplementationOnce((): PromiseLike<Partial<QueryResult>> =>
+                        Promise.resolve({
+                            rowCount: 1,
+                            rows: [{
+                                'id': BOOK_ID
+                            }]
+                        })
+                    ).mockImplementation(() => Promise.reject('mock not implemented'));
 
-            // call save
-            const dao = daoFactory<_book>(_book, pool);
+                // call save
+                const dao = daoFactory<_book>(_book, pool);
 
-            let book = new _book();
-            book.pages.push(new _page())
-            book.pages.push(new _page())
+                let book = new _book();
+                book.author = undefined;
 
-            book = (await dao.save(book))!;
-            expect(book).toBeDefined();
-            expect(book.id).toBe(BOOK_ID);
-            expect(book.pages![0]!.id).toBe(PAGE_ID);
-            expect(book.pages![0]!.bookId).toBe(BOOK_ID);
-            expect(book.pages![1]!.id).toBe(PAGE_ID_2);
-            expect(book.pages![1]!.bookId).toBe(BOOK_ID);
+                book = (await dao.save(book))!;
+                expect(book).toBeDefined();
+                expect(book.id).toBe(BOOK_ID);
+                expect(book.author).toBeUndefined();
+            })
+
+            test("error saving join entity returns undefined", async () => {
+                // mocked saves to return id 1
+                const BOOK_ID = 2;
+                jest.spyOn(pool, 'query')
+                    .mockImplementationOnce((): PromiseLike<Partial<QueryResult>> =>
+                        Promise.resolve({
+                            rowCount: 1,
+                            rows: [{
+                                'id': BOOK_ID
+                            }]
+                        })
+                        // stop author from being saved
+                    ).mockImplementation(() => Promise.reject('mock breaking author save'));
+
+                // call save
+                const dao = daoFactory<_book>(_book, pool);
+
+                let book = new _book();
+                book.author = new _author();
+
+                book = (await dao.save(book))!;
+                expect(book).toBeUndefined();
+            })
+
+        });
+        describe("multiple association", () => {
+            test("id set on joined entities", async () => {
+                // mocked saves to return id 1
+                const BOOK_ID = 1;
+                const PAGE_ID = 2;
+                const PAGE_ID_2 = 3;
+                jest.spyOn(pool, 'query')
+                    .mockImplementationOnce((): PromiseLike<Partial<QueryResult>> =>
+                        Promise.resolve({
+                            rowCount: 1,
+                            rows: [{
+                                'id': BOOK_ID
+                            }]
+                        })
+                    )
+                    .mockImplementationOnce((): PromiseLike<Partial<QueryResult>> =>
+                        Promise.resolve({
+                            rowCount: 1,
+                            rows: [{
+                                'id': PAGE_ID
+                            }]
+                        })
+                    )
+                    .mockImplementationOnce((): PromiseLike<Partial<QueryResult>> =>
+                        Promise.resolve({
+                            rowCount: 1,
+                            rows: [{
+                                id: PAGE_ID_2
+                            }]
+                        })
+                    ).mockImplementation(() => { Promise.reject('mock not implemented') })
+
+                // call save
+                const dao = daoFactory<_book>(_book, pool);
+
+                let book = new _book();
+                book.author = undefined;
+                book.pages.push(new _page())
+                book.pages.push(new _page())
+
+                book = (await dao.save(book))!;
+                expect(book).toBeDefined();
+                expect(book.id).toBe(BOOK_ID);
+                expect(book.pages![0]!.id).toBe(PAGE_ID);
+                expect(book.pages![0]!.bookId).toBe(BOOK_ID);
+                expect(book.pages![1]!.id).toBe(PAGE_ID_2);
+                expect(book.pages![1]!.bookId).toBe(BOOK_ID);
+            })
         })
     })
 })
