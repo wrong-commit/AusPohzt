ffd9231669d59c9932eb3bef8b165c46beb20c9c
quinn <quinn.samuel.perry@gmail.com>
2022-07-03 15:54:38 +1000

    Cleanup dao
    Now takes pool as required constructor argument, allows for easy mocking
    Cache getFields response when constructor called
    Break out join save code into separate methods
    Add more logging to dao.delete()
    Setup dao unit test with mocked pg.pool to test generic dao mappings

--- src/dao/daoFactory.ts
+++ src/dao/daoFactory.ts
@@ -1,3 +1,5 @@
+import { Pool } from "pg";
+import { pool } from "../database/database";
 import { getDao } from "../decorator/daoDecorators";
 import { parcel } from "../entities/parcel";
 import { Newable } from "../types/Newable";
@@ -6,15 +8,18 @@ import { parcelDao } from "./parcelDao";
 
 export { daoFactory }
 
-new parcelDao(parcel);
+new parcelDao(parcel, pool);
 
 function daoFactory(x: Newable<parcel>): parcelDao;
+function daoFactory(x: Newable<parcel>, overridePool: Pool): parcelDao;
+function daoFactory<T extends daoEntity>(x: Newable<T>): dao<T>;
+function daoFactory<T extends daoEntity>(x: Newable<T>, overridePool: Pool): dao<T>;
 
-function daoFactory<T extends daoEntity>(x: Newable<T>): dao<T> {
+function daoFactory<T extends daoEntity>(x: Newable<T>, overridePool?: Pool): dao<T> {
     let entityDao = getDao<T>(x);
     if (!entityDao) {
-        return new baseDao(x);
+        return new baseDao(x, overridePool ?? pool);
     };
 
-    return new entityDao(x);
+    return new entityDao(x, overridePool ?? pool);
 }
