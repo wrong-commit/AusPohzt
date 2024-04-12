d913075d56d9f2cc6ec83851aa5d59410facf072
wrong-commit <quinn.samuel.perry@gmail.com>
2024-04-12 17:52:42 +1000

    Add more headers to skip presumed bot checks

--- backend/src/clients/auspost.ts
+++ backend/src/clients/auspost.ts
@@ -28,7 +28,11 @@ class auspost implements client<shipmentsResponse> {
                     Host: 'digitalapi.auspost.com.au',
                     Connection: 'keep-alive',
                     Accept: 'application/json, text/plain, */*',
-                    'Accept-Language': 'en-US,en;q=0.9',
+                    'Accept-Language': 'en-US,en;q=0.5',
+                    'Accept-Encoding':'gzip, deflate, br',
+                    'AP_CHANNEL_NAME': 'WEB_DETAIL',
+                    'api-key': 'd11f9456-11c3-456d-9f6d-f7449cb9af8e',
+                    'User-Agent' :'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0'
                 },
                 // error if 200 not received
                 statusCode: 200,
