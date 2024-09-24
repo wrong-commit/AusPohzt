6ed81f912f6f30682e41ff6354b39a20d553b8ab
wrong-commit <quinn.samuel.perry@gmail.com>
2024-09-24 16:56:08 +1000

    Front end builds in Docker
    Front end complains that process.env is not replaced during the webpack build

---
+++ frontend/src/environment.d.ts
@@ -0,0 +1,14 @@
+
+declare global {
+    namespace NodeJS {
+        // TODO: add method to ensure environment variables are set, or set default if required.
+        interface ProcessEnv {
+            /**
+             * Enable or disable authentication.
+             */
+            ENABLE_AUTH: 'true' | 'false';
+            API_HOST: string;
+        }
+    }
+}
+export { }
