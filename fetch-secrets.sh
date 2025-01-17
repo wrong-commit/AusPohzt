301899018044cbc750469650275b14e5765567b8
wrong-commit <quinn.samuel.perry@gmail.com>
2025-01-17 17:26:08 +1100

    Dynamically fetch AWS Secrets Manager name Always rebuild FE on Auspohzt service restart

--- fetch-secrets.sh
+++ fetch-secrets.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Fetch secrets from AWS Secrets Manager
-SECRET_NAME="pik-manual-test"
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` 
+SECRET_NAME=`curl --fail -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/tags/instance/AWS_Secrets_Manager`
+echo $SECRET_NAME
+if [[ -z "${SECRET_NAME}" ]]; then 
+    echo Could not fetch SECRET_NAME for AWS EC2 tags. 
+    exit 1
+fi
 SECRET_VALUE=$(aws secretsmanager get-secret-value --secret-id "$SECRET_NAME" --query SecretString --output text)
 
 # Parse JSON and export to .env
